62f496e482d340b87c7f4eeaf1679fd5cd880169
Marcus Rodrigues <markynrodrisilva@gmail.com>
2022-11-16 19:42:36 -0300

    changing content from ./src/ to ./dist/

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: { fontFamily: {
       Montserrat: ['Montserrat', "sans-serif"],
